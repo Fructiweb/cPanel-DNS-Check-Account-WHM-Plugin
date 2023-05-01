df62290a01823951c2702c153c862173dcfcc5c8
Jordan <jordan.roucomiguez@digitalsavior.fr>
2023-05-01 23:04:56 +0200

    trying to display header and footer

--- dns-check.php
+++ dns-check.php
@@ -1,4 +1,10 @@
 <?php
+
+include_once('/usr/local/cpanel/php/WHM.php');
+if (class_exists(WHM::class)) {
+	WHM::header('Example Plugin Interface', 0, 0);
+}
+
 $localdomain = '/etc/localdomains';
 $userdatadomains = '/etc/userdatadomains';
 $ini_set = 0;
@@ -58,8 +64,10 @@ function check_valid_resolve_ip($ip, $domain)
 function get_domain_ip_local_file($domain)
 {
 	global $userdatadomains;
+
 	$file_lines = open_file_per_line($userdatadomains);
 	$file_ip_nat_lines = open_file_per_line('/var/cpanel/cpnat');
+	$file_ip_nat_lines = is_bool($file_ip_nat_lines) ? [] : $file_ip_nat_lines;
 	foreach ($file_lines as $line) {
 		$explode = explode('==', $line);
 		$explode_two = explode(':', $explode[0]);
@@ -81,71 +89,76 @@ function get_domain_ip_local_file($domain)
 $all_domains_local = open_file_per_line($localdomain);
 $hostname = gethostname();
 ?>
-<html>
-<head>
-	<title>DNS Check Account</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/darkly/bootstrap.min.css"
-	      integrity="sha384-nNK9n28pDUDDgIiIqZ/MiyO3F4/9vsMtReZK39klb/MtkZI3/LtjSjlmyVPS3KdN" crossorigin="anonymous">
-</head>
-<body>
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<h1>cPanel DNS Check Account WHM Plugin</h1>
+	<html>
+	<head>
+		<title>DNS Check Account</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/darkly/bootstrap.min.css"
+		      integrity="sha384-nNK9n28pDUDDgIiIqZ/MiyO3F4/9vsMtReZK39klb/MtkZI3/LtjSjlmyVPS3KdN"
+		      crossorigin="anonymous">
+	</head>
+	<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>cPanel DNS Check Account WHM Plugin</h1>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<table class="table">
-				<thead>
-				<tr>
-					<td>User</td>
-					<td>Reseller User</td>
-					<td>Domain</td>
-					<td>Local IP</td>
-					<td></td>
-				</tr>
-				</thead>
-				<tbody>
-				<?php
-				foreach ($all_domains_local as $domain) {
-					$domain_local_acc = get_domain_ip_local_file($domain);
-					$resolve_ips = resolve_domain($domain);
-					$ips_ = '';
-					if ($domain_local_acc['type'] === 'sub') {
-						continue;
-					}
-					foreach ($resolve_ips as $ip) {
-						if ($domain === $hostname) {
-							$domain = '_SERVER_HOSTNAME_';
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<table class="table">
+					<thead>
+					<tr>
+						<td>User</td>
+						<td>Reseller User</td>
+						<td>Domain</td>
+						<td>Local IP</td>
+						<td></td>
+					</tr>
+					</thead>
+					<tbody>
+					<?php
+					foreach ($all_domains_local as $domain) {
+						$domain_local_acc = get_domain_ip_local_file($domain);
+						$resolve_ips = resolve_domain($domain);
+						$ips_ = '';
+						if ($domain_local_acc['type'] === 'sub') {
+							continue;
 						}
-						$check = check_valid_resolve_ip($ip, $domain);
-						$ips_ .= '<span class="alert alert-' . $check['label'] . '">' . $ip . '</span> ' . $check['msg'] . '<br><br>';
-					}
-					$ips = rtrim($ips_, '<br>');
-					$ip_result_html = $ips !== '' ? $ips : '<span class="alert alert-danger">Not Resolve</span>';
-					if ($domain === '_SERVER_HOSTNAME_') {
-						$domain = $hostname;
-						$domain_local_acc['acc'] = 'root';
+						foreach ($resolve_ips as $ip) {
+							if ($domain === $hostname) {
+								$domain = '_SERVER_HOSTNAME_';
+							}
+							$check = check_valid_resolve_ip($ip, $domain);
+							$ips_ .= '<span class="alert alert-' . $check['label'] . '">' . $ip . '</span> ' . $check['msg'] . '<br><br>';
+						}
+						$ips = rtrim($ips_, '<br>');
+						$ip_result_html = $ips !== '' ? $ips : '<span class="alert alert-danger">Not Resolve</span>';
+						if ($domain === '_SERVER_HOSTNAME_') {
+							$domain = $hostname;
+							$domain_local_acc['acc'] = 'root';
+						}
+						?>
+						<tr>
+							<td><?= $domain_local_acc['acc'] ?></td>
+							<td><?= $domain_local_acc['reseller'] ?></td>
+							<td>(<?= $domain_local_acc['type'] ?>) <?= $domain ?></td>
+							<td><?= $domain_local_acc['ip'] ?></td>
+							<td><?= $ip_result_html ?><br></td>
+						</tr>
+						<?php
 					}
 					?>
-					<tr>
-						<td><?= $domain_local_acc['acc'] ?></td>
-						<td><?= $domain_local_acc['reseller'] ?></td>
-						<td>(<?= $domain_local_acc['type'] ?>) <?= $domain ?></td>
-						<td><?= $domain_local_acc['ip'] ?></td>
-						<td><?= $ip_result_html ?><br></td>
-					</tr>
-					<?php
-				}
-				?>
-				</tbody>
-			</table>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
-</div>
-</body>
-</html>
+	</body>
+	</html>
+<?php
+if (class_exists(WHM::class)) {
+	WHM::footer();
+}
