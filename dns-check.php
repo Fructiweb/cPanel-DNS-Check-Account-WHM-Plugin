0ad49576d1c5b3f15a16cffa137a7765ca9b980f
Jordan <jordan.roucomiguez@digitalsavior.fr>
2023-05-01 23:14:30 +0200

    Update dns-check.php

--- dns-check.php
+++ dns-check.php
@@ -153,9 +153,11 @@ $hostname = gethostname();
 		</div>
 	</div>
 </div>
+<div style="background-color: white !important;">
 <?php
 if (class_exists(WHM::class)) {
 	WHM::footer();
 }
 ?>
+</div>
 </body>
