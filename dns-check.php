29fb0894203be6ace0a25e5d0b7c6746291e26ad
Jordan <jordan.roucomiguez@digitalsavior.fr>
2023-07-27 08:57:01 +0200

    Update dns-check.php

--- dns-check.php
+++ dns-check.php
@@ -2,7 +2,7 @@
 
 include_once('/usr/local/cpanel/php/WHM.php');
 if (class_exists(class: WHM::class)) {
-	WHM::header('', 0, 0);
+    WHM::header('', 0, 0);
 }
 
 $localdomain = '/etc/localdomains';
@@ -10,80 +10,80 @@ $userdatadomains = '/etc/userdatadomains';
 $ini_set = 0;
 
 if (file_exists('.env')) {
-	$localdomain = 'localdomains';
-	$userdatadomains = 'userdatadomains';
-	$ini_set = 1;
+    $localdomain = 'localdomains';
+    $userdatadomains = 'userdatadomains';
+    $ini_set = 1;
 }
 
 ini_set('display_errors', $ini_set);
 
 function resolve_domain($domain): array
 {
-	$dns = '8.8.8.8';  // Google Public DNS
-	if (rand(0, 1) == 1) {
-		$dns = '208.67.222.222'; // Open DNS
-	}
-	$ip = `nslookup $domain $dns`; // the backticks execute the command in the shell
-	$ips = [];
-	if (preg_match_all('/Address: ((?:\d{1,3}\.){3}\d{1,3})/', $ip, $match) > 0) {
-		$ips = $match[1];
-	}
-	return $ips;
+    $dns = '8.8.8.8';  // Google Public DNS
+    if (rand(0, 1) == 1) {
+        $dns = '208.67.222.222'; // Open DNS
+    }
+    $ip = `nslookup $domain $dns`; // the backticks execute the command in the shell
+    $ips = [];
+    if (preg_match_all('/Address: ((?:\d{1,3}\.){3}\d{1,3})/', $ip, $match) > 0) {
+        $ips = $match[1];
+    }
+    return $ips;
 }
 
 function open_file_per_line($file): bool|array
 {
-	$handle = fopen($file, "r");
-	if ($handle) {
-		$lines = [];
-		while (($line = fgets($handle)) !== false) {
-			$lines[] = trim($line);
-		}
-		return $lines;
-		fclose($handle);
-	} else {
-		return false;
-	}
+    $handle = fopen($file, "r");
+    if ($handle) {
+        $lines = [];
+        while (($line = fgets($handle)) !== false) {
+            $lines[] = trim($line);
+        }
+        return $lines;
+        fclose($handle);
+    } else {
+        return false;
+    }
 }
 
 function check_valid_resolve_ip($ip, $domain): array
 {
-	if ($domain === '_SERVER_HOSTNAME_') {
-		return ['label' => 'info', 'msg' => ''];
-	}
-	if (!filter_var($ip, FILTER_VALIDATE_IP)) {
-		return ['label' => 'danger', 'msg' => 'Invalid IP'];
-	}
-	$domain_local_ip = get_domain_ip_local_file($domain);
-	if ($domain_local_ip['ip'] !== $ip) {
-		return ['label' => 'danger', 'msg' => 'Different IP'];
-	}
-	return ['label' => 'success', 'msg' => ''];
+    if ($domain === '_SERVER_HOSTNAME_') {
+        return ['label' => 'info', 'msg' => ''];
+    }
+    if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+        return ['label' => 'danger', 'msg' => 'Invalid IP'];
+    }
+    $domain_local_ip = get_domain_ip_local_file($domain);
+    if ($domain_local_ip['ip'] !== $ip) {
+        return ['label' => 'danger', 'msg' => 'Different IP'];
+    }
+    return ['label' => 'success', 'msg' => ''];
 }
 
 function get_domain_ip_local_file($domain): array
 {
-	global $userdatadomains;
+    global $userdatadomains;
 
-	$file_lines = open_file_per_line($userdatadomains);
-	$file_ip_nat_lines = open_file_per_line('/var/cpanel/cpnat');
-	$file_ip_nat_lines = is_bool($file_ip_nat_lines) ? [] : $file_ip_nat_lines;
-	foreach ($file_lines as $line) {
-		$explode = explode('==', $line);
-		$explode_two = explode(':', $explode[0]);
-		if (trim($explode_two[0]) === trim($domain)) {
-			$ip_port = $explode[5];
-			$explode_ip = explode(':', $ip_port);
-			foreach ($file_ip_nat_lines as $line_ip_nat) {
-				$explode_ip_nat = explode(' ', $line_ip_nat);
-				if ($explode_ip_nat[0] == $explode_ip[0]) {
-					$explode_ip[0] = $explode_ip_nat[1];
-				}
-			}
-			return ['ip' => $explode_ip[0], 'acc' => trim($explode_two[1]), 'reseller' => trim($explode[1]), 'type' => trim($explode[2])];
-		}
-	}
-	return [];
+    $file_lines = open_file_per_line($userdatadomains);
+    $file_ip_nat_lines = open_file_per_line('/var/cpanel/cpnat');
+    $file_ip_nat_lines = is_bool($file_ip_nat_lines) ? [] : $file_ip_nat_lines;
+    foreach ($file_lines as $line) {
+        $explode = explode('==', $line);
+        $explode_two = explode(':', $explode[0]);
+        if (trim($explode_two[0]) === trim($domain)) {
+            $ip_port = $explode[5];
+            $explode_ip = explode(':', $ip_port);
+            foreach ($file_ip_nat_lines as $line_ip_nat) {
+                $explode_ip_nat = explode(' ', $line_ip_nat);
+                if ($explode_ip_nat[0] == $explode_ip[0]) {
+                    $explode_ip[0] = $explode_ip_nat[1];
+                }
+            }
+            return ['ip' => $explode_ip[0], 'acc' => trim($explode_two[1]), 'reseller' => trim($explode[1]), 'type' => trim($explode[2])];
+        }
+    }
+    return [];
 }
 
 $all_domains_local = open_file_per_line($localdomain);
@@ -95,62 +95,64 @@ $hostname = gethostname();
       integrity="sha384-nNK9n28pDUDDgIiIqZ/MiyO3F4/9vsMtReZK39klb/MtkZI3/LtjSjlmyVPS3KdN"
       crossorigin="anonymous">
 <body>
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<h1>cPanel DNS Check Account WHM Plugin</h1>
-		</div>
-	</div>
-</div>
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<table class="table">
-				<thead>
-				<tr>
-					<td>User</td>
-					<td>Reseller User</td>
-					<td>Domain</td>
-					<td>Local IP</td>
-					<td></td>
-				</tr>
-				</thead>
-				<tbody>
-				<?php
-				foreach ($all_domains_local as $domain) {
-					$domain_local_acc = get_domain_ip_local_file($domain);
-					$resolve_ips = resolve_domain($domain);
-					$ips_ = '';
-					if ($domain_local_acc['type'] === 'sub' || empty($domain_local_acc['acc'])) {
-						continue;
-					}
-					foreach ($resolve_ips as $ip) {
-						if ($domain === $hostname) {
-							$domain = '_SERVER_HOSTNAME_';
-						}
-						$check = check_valid_resolve_ip($ip, $domain);
-						$ips_ .= '<span class="alert alert-' . $check['label'] . '">' . $ip . '</span> ' . $check['msg'] . '<br><br>';
-					}
-					$ips = rtrim($ips_, '<br>');
-					$ip_result_html = $ips !== '' ? $ips : '<span class="alert alert-danger">Not Resolve</span>';
-					if ($domain === '_SERVER_HOSTNAME_') {
-						$domain = $hostname;
-						$domain_local_acc['acc'] = 'root';
-					}
-					?>
-					<tr>
-						<td><?= $domain_local_acc['acc'] ?></td>
-						<td><?= $domain_local_acc['reseller'] ?></td>
-						<td>(<?= $domain_local_acc['type'] ?>) <?= $domain ?></td>
-						<td><?= $domain_local_acc['ip'] ?></td>
-						<td><?= $ip_result_html ?><br></td>
-					</tr>
-					<?php
-				}
-				?>
-				</tbody>
-			</table>
-		</div>
-	</div>
-</div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>cPanel DNS Check Account WHM Plugin</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <td>User</td>
+                        <td>Reseller User</td>
+                        <td>Domain</td>
+                        <td>Local IP</td>
+                        <td></td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach ($all_domains_local as $domain) {
+                        if ($domain_local_acc['type'] === 'sub' || $domain_local_acc['type'] === 'main' || empty($domain_local_acc['acc'])) {
+                            continue;
+                        }
+
+                        $domain_local_acc = get_domain_ip_local_file($domain);
+                        $resolve_ips = resolve_domain($domain);
+                        $ips_ = '';
+
+                        foreach ($resolve_ips as $ip) {
+                            if ($domain === $hostname) {
+                                $domain = '_SERVER_HOSTNAME_';
+                            }
+                            $check = check_valid_resolve_ip($ip, $domain);
+                            $ips_ .= '<span class="alert alert-' . $check['label'] . '">' . $ip . '</span> ' . $check['msg'] . '<br><br>';
+                        }
+                        $ips = rtrim($ips_, '<br>');
+                        $ip_result_html = $ips !== '' ? $ips : '<span class="alert alert-danger">Not Resolve</span>';
+                        if ($domain === '_SERVER_HOSTNAME_') {
+                            $domain = $hostname;
+                            $domain_local_acc['acc'] = 'root';
+                        }
+                        ?>
+                        <tr>
+                            <td><?= $domain_local_acc['acc'] ?></td>
+                            <td><?= $domain_local_acc['reseller'] ?></td>
+                            <td>(<?= $domain_local_acc['type'] ?>) <?= $domain ?></td>
+                            <td><?= $domain_local_acc['ip'] ?></td>
+                            <td><?= $ip_result_html ?><br></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
