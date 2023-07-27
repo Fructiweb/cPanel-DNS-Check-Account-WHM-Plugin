86641c9d163fb9b11f0d2985bbc03c8779742f25
Jordan <jordan.roucomiguez@digitalsavior.fr>
2023-07-27 09:56:45 +0200

    Update dns-check.php

--- dns-check.php
+++ dns-check.php
@@ -160,6 +160,7 @@ $hostname = gethostname();
                             $shell_command_output = json_decode($shell_command_output, true);
                             $default_domain_name = parse_url($shell_command_output['data']['url'], PHP_URL_HOST);
                             $login_link = str_replace($default_domain_name, $fructiweb_cloud_domain, $shell_command_output['data']['url']);
+                            var_dump($login_link);
                             $login_link = '<a class="btn btn-info rounded" href="' . $login_link . '" target="_blank">Connexion</a>';
                         }
                         ?>
