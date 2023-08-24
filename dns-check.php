0553cfa5d0ee933267a4fd20e4572577d65d66e4
Jordan <jordan.roucomiguez@digitalsavior.fr>
2023-08-24 19:28:41 +0200

    Update dns-check.php

--- dns-check.php
+++ dns-check.php
@@ -1,9 +1,11 @@
 <?php
+ob_start();
 @include_once('/usr/local/cpanel/php/WHM.php');
 if (class_exists(class: WHM::class)) {
 	WHM::header('', 0, 0);
 }
 
+ob_clean();
 ob_start();
 
 if ($cache_static = @file_get_contents('dns-check.html')) {
