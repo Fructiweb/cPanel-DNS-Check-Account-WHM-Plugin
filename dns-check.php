080043ba8e2d6808f5572c7367df737a362f681a
Jordan <jordan.roucomiguez@digitalsavior.fr>
2023-11-07 09:12:00 +0100

    Update dns-check.php

--- dns-check.php
+++ dns-check.php
@@ -193,6 +193,8 @@ $hostname = gethostname();
 							$login_link = '';
 							$shell_link_command_output = shell_exec('/usr/local/cpanel/bin/whmapi1 --output=jsonpretty create_user_session user=' . $domain_local_acc['acc'] . ' service=cpaneld');
 
+							$domain_local_acc['sub'] = strstr($domain_local_acc['sub'], 'cloud');
+                            
 							$pattern_cloud = "/cloud\d+/";
 							$pattern_domain = "/(\w+)fructiweb/";
 							$replacement_cloud = "$0.";
